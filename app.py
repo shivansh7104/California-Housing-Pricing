32bbe47263770e88da1b41bdb1bf6081a57fd2bc
Shivansh <shivanshmishra7104@gmail.com>
2026-05-31 17:55:08 +0530

    This commit is for web application

---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask, request, app, jsonify, render_template, url_for, redirect
+import numpy as np
+import pandas as pd
+
+
+
+app=Flask(__name__)
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaler.pkl','rb'))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+
+@app.route("/predict_api",methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__=="__main__":
+    app.run(debug=True)
